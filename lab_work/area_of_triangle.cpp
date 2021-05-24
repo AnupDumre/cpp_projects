c9edb2b3c800cd529f00cd9be20680518b56c2a2
Anup Dumre <anupabde17@gmail.com>
2021-05-24 22:45:06 +0545

    lab_#include<iostream>

---
+++ lab_work/area_of_triangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{   
+    float base, height, area ;
+    cout<<"\tEnter the base of the triangle : ";
+    cin>>base;
+    cout<<"\n\tEnter the height of the triangle : ";
+    cin>>height;
+    area = (0.5)*base*height;
+    cout<<"\t\nThe area of the triangle is : "<<area;
+    return 0;
+
+
+}
