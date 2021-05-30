18ef30c60e644e90be54e18ccc370e1dfd0898e7
Anup Dumre <anupabde17@gmail.com>
2021-05-30 21:36:42 +0545

    area of triangle

--- Assignment/1_Area_of_Triangle/area_of_triangle.cpp
+++ Assignment/1_Area_of_Triangle/area_of_triangle.cpp
@@ -8,6 +8,6 @@ int main()
     cout<<"\tEnter the height of the triangle : ";
     cin>>height;
     area = (0.5)*base*height;
-    cout<<"\tThe area of the triangle is :"<<area<<endl;
+    cout<<"\tThe area of the triangle is : "<<area<<endl;
     return 0;
 }
