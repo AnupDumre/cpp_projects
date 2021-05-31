43657e8f922c669f36c3378ed68d0a44eed6d028
Anup Dumre <anupabde17@gmail.com>
2021-05-31 16:25:21 +0545

    To get the sum of first 'n' natural number

--- Assignment/Assignment2_Condition_and_Switch/6_sum_of_first_n_natural_number.cpp
+++ Assignment/Assignment2_Condition_and_Switch/6_sum_of_first_n_natural_number.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main()
 {
     int a,sum=0,n=1;
-    cout<<"Enter the number of terms you want to get sum:";
+    cout<<"\tEnter the number of terms you want to get sum=";
     cin>>a;
     while(n<=a)
     {
         sum=sum+n;
         n++;
     }
-    cout<<"The sum of number of first " << a << " natural number = "<<sum<<endl;
+    cout<<"\tThe sum of number of first " << a << " natural number = "<<sum<<endl;
     return (0);
 }
