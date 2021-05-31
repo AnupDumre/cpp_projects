8feeb39ae568e855d85b65503957a5cfae9dd29f
Anup Dumre <anupabde17@gmail.com>
2021-05-31 16:30:52 +0545

    lab work of structure

--- lab_work/lab_2/executing_struct.cpp
+++ lab_work/lab_2/executing_struct.cpp
@@ -13,20 +13,20 @@ int main(){
     student s[3];
     for(int i = 0; i < 3; i++)
     {
-    cout<<"Enter the details of the student "<<i+1<<": "<<endl;
+    cout<<"\tEnter the details of the student "<<i+1<<": "<<endl;
     cout<<"\tName:";
     cin>> s[i].name;
-    cout<<"Roll no.";
+    cout<<"\tRoll no.";
     cin>> s[i].roll;
-    cout<<"Address:";
+    cout<<"\tAddress:";
     cin>> s[i].address;
     }
     for (int i=0;i<3;i++)
     {
-    cout<<"\n\tThe detail of the student is:"<<i+1<<":"<<endl;
-    cout<<"\tName:"<<s[i].name;
-    cout<<"\tRoll no."<<s[i].roll;
-    cout<<"\taddress"<<s[i].address;
+    cout<<"\n\tThe detail of the student is: "<<i+1<<" : "<<endl;
+    cout<<"\tName: "<<s[i].name;
+    cout<<"\tRoll no. "<<s[i].roll;
+    cout<<"\tAddress "<<s[i].address;
     }
     return 0;
     
