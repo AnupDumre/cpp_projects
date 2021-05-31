f491ff9df6f6ca830fce71afbaaa25c9425a31bb
Anup Dumre <anupabde17@gmail.com>
2021-05-31 13:40:54 +0545

    To print the positive number

---
+++ Assignment/Assignment2_Condition_and_Switch/2_check_positive_or_negative.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+	int number;
+    cout<<"\n\tEnter any number:"<<number;
+    cin>>number;
+    if(number>=0)
+    {
+        cout<<"\n\tThe entered number is positive."<<endl;
+    }    
+    else
+    {    
+        cout<<"\n\tThe entered number is negative."<<endl;
+    }
+    return 0;
+}
