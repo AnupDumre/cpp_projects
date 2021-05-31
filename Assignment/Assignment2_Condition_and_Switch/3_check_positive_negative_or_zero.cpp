8ed8b7b9b6723f07277d281882bfbbb08e9bb3d5
Anup Dumre <anupabde17@gmail.com>
2021-05-31 15:09:00 +0545

    To check the number positive negative or zero

--- Assignment/Assignment2_Condition_and_Switch/3_check_positive_negative_or_zero.cpp
+++ Assignment/Assignment2_Condition_and_Switch/3_check_positive_negative_or_zero.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main()
 {
 	int number;
-    cout<<"\n\t Enter any number:"<<number;
+    cout<<"\n\t Enter any number="<<number;
     cin>>number;
     if(number>0)
     {
-        cout<<"\n\t The entered number is positive."<<endl;
+        cout<<"\n\tThe entered number is positive."<<endl;
     }    
     else if(number<0)
     {
-        cout<<"\n\t The entered number is negative."<<endl;
+        cout<<"\n\tThe entered number is negative."<<endl;
     }
     else
     {    
-        cout<<"\n\t The entered number is zero."<<endl;
+        cout<<"\n\tThe entered number is zero."<<endl;
     }
     return 0;
 }
