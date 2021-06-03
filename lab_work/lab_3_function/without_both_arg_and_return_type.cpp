f8892bdc3f3ca886cd0e77293af80bed74d3bf71
Anup Dumre <anupabde17@gmail.com>
2021-06-03 21:21:51 +0545

    Program of function without both arguments and return types.

--- lab_work/lab_3_function/without_both_arg_and_return_type.cpp
+++ lab_work/lab_3_function/without_both_arg_and_return_type.cpp
@@ -2,7 +2,7 @@
 #include<conio.h>
 #include<math.h>
 using namespace std;
-void area_of_triangle ();
+void area_of_triangle();
 int main()
 {
     area_of_triangle();
@@ -12,11 +12,11 @@ void area_of_triangle()
 {
     float s1,s2,s3,s,area;
     cout<<"\tEnter three side of the triangle: "<<endl;
-    cout<<"\tSide 1: ";
+    cout<<"\tSide 1 : ";
     cin>>s1;
-    cout<<"\tSide 2: ";
+    cout<<"\tSide 2 : ";
     cin>>s2;
-    cout<<"\tSide 3: ";
+    cout<<"\tSide 3 : ";
     cin>>s3;
     s=(s1+s2+s3)/2;
     area= sqrt(s*(s-s1)*(s-s2)*(s-s3));
