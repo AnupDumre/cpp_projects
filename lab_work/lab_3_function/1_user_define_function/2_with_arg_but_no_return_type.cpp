358266667c878850310cc3216c46a8d65d55811d
Anup Dumre <anupabde17@gmail.com>
2021-06-08 19:04:05 +0545

    Program of function with argument but no return type

---
+++ lab_work/lab_3_function/1_user_define_function/2_with_arg_but_no_return_type.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<conio.h>
+#include<math.h>
+using namespace std;
+
+void area_of_triangle(float, float, float);
+
+int main()
+{
+    float side_1, side_2, side_3;
+    cout<<"\tEnter three side of the triangle:"<<endl;
+    cout<<"\tside 1 ";
+    cin>>side_1;
+    cout<<"\tside 2 ";
+    cin>>side_2;
+    cout<<"\tside 3 ";
+    cin>>side_3;
+
+    area_of_triangle(side_1, side_2, side_3);
+    return 0;
+}
+
+void area_of_triangle(float s1, float s2, float s3)
+{
+    float s,area;
+    s = (s1 + s2 + s3)/2;
+    area = sqrt(s*(s-s1)*(s-s2)*(s-s3));
+    cout<<"\tThe area of triangle is: "<<area<<endl;
+}
