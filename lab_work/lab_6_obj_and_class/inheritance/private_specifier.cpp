c6942ab2fb9c1ad9e89ba869b22fc7e5dc64eb1d
Anup Dumre <anupabde17@gmail.com>
2021-07-10 21:10:44 +0545

    Program ofobject and class using pointers

---
+++ lab_work/lab_6_obj_and_class/inheritance/private_specifier.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+class circle{
+        private:
+            double radius;
+        public:
+            void input(double r)
+            {
+                radius=r;
+            }
+            double compute_area()
+            {
+                return 3.1415*radius*radius;
+            }
+};
+int main()
+{
+    circle obj;
+    obj.input(7.5);
+    //cout<<"radius is:"<<obj.r<<endl;
+    cout<<"area is:"<<obj.compute_area()<<endl;
+    return 0;
+}
