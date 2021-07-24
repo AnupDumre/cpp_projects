099b1f2b87537395e31b91f6ecdac7841f9a266c
Anup Dumre <anupabde17@gmail.com>
2021-07-24 21:23:56 +0545

    program of template overloading

---
+++ lab_work/lab_10_template/4_overloading_temp_template.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+template <class T> //first template
+void func(T a, T b)//here both arguments are of same datatpes
+{
+    cout<<"func(T a , T b)-->\t"<<a<<" "<<b<<endl;
+}
+template<class T1, class T2>//second template
+void func(T1 a, T2 b) //here arguments are of different data types
+{
+    cout<<"func(T1 a , T2 b)-->\t"<<a<<" "<<b<<endl;
+}
+int main()
+{
+    func(28,36);//first template function is called
+    func(22,3.6);//second template function is called
+    func("We","live");//first template function is called
+    func("Mt Everest",8848);//second template function is called
+    return 0;   
+}
+/* OUTPUT
+func(T a , T b)-->	28 36
+func(T1 a , T2 b)-->	22 3.6
+func(T a , T b)-->	We live
+func(T1 a , T2 b)-->	Mt Everest 8848
+*/
