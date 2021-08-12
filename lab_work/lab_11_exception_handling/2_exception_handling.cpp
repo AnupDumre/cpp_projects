f84d76182315bb71564b23f39e998a4bc1728920
Anup Dumre <anupabde17@gmail.com>
2021-08-12 07:23:44 +0545

    Program of exception handling

---
+++ lab_work/lab_11_exception_handling/2_exception_handling.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter the value of a and b: " << endl;
+    cin >> a >> b;
+    int x = (a - b);
+    try
+    {
+        if (x != 0)
+        {
+            cout << "output is " << a / (a - b) << endl;
+        }
+        else
+        {
+            throw(x);
+        }
+    }
+    catch (int x)
+    {
+
+        cout << "Division by 0 is not allowed!!! " << endl;
+    }
+    cout << "Next statement" << endl;
+    return 0;
+}
+
+/* OUTPUT 
+
+Enter the value of a and b:
+28 06
+output is 1
+Next statement
+
+*/
